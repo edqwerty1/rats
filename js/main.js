6e283149d79816d01d3dc7b5a0b07130238c6d4e
Edward Blacklee <Ed.blacklee@outlook.com>
2018-03-09 16:36:07 +0000

    added initial project

---
+++ js/main.js
@@ -0,0 +1,50 @@
+﻿(function (main) {
+
+    main.init = function () {
+        var searchBox = document.getElementById("map-search");
+        searchBox.onkeyup = main.mapSearch;
+        searchBox.change = main.mapSearch;
+
+        var nav = document.getElementById("map-navigation");
+        nav.innerHTML = navHtml;
+    };
+
+    main.mapSearch = function (event) {
+        var map = maps.list.find((element) => { return element.name.startsWith(event.target.value); });
+
+        var imageHtml = "";
+        if (!event.target.value || !map) {
+            var insert = document.getElementById("map-insert");
+            insert.innerHTML = imageHtml;
+            return;
+        }
+
+        map.images.forEach((element) => {
+
+            imageHtml += `
+<div>
+    <div>${element.title}</div>
+    <div>${element.description}</div>
+    <image class="map-image" src="${element.url}">
+</div>
+`;
+        });
+
+        var insert = document.getElementById("map-insert");
+        insert.innerHTML = imageHtml;
+    };
+
+    // Nav Bar
+    navHtml = "";
+    maps.list.forEach((element) => {
+
+        navHtml += `
+<a href="www.google.com">
+   ${element.name}
+</a>
+`;
+    });
+
+
+    main.init();
+})(window.main = window.main || {});
---
+++ js/maps.js
@@ -0,0 +1,38 @@
+﻿(function (maps) {
+
+    maps.list = [{
+        name: "test", images: [{
+            title: "Title 1",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+            url: "https://images.pexels.com/photos/53545/background-blue-clean-clear-53545.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+        },
+        {
+            title: "Title 2",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+            url: "https://images.pexels.com/photos/53545/background-blue-clean-clear-53545.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+        },
+        {
+            title: "Title 3",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+            url: "https://images.pexels.com/photos/53545/background-blue-clean-clear-53545.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+        }]
+    },
+        {
+            name: "asasd", images: [{
+                title: "Title 1",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+                url: "https://images.pexels.com/photos/53545/background-blue-clean-clear-53545.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+            },
+            {
+                title: "Title 2",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+                url: "https://images.pexels.com/photos/53545/background-blue-clean-clear-53545.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+            },
+            {
+                title: "Title 3",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+                url: "https://images.pexels.com/photos/53545/background-blue-clean-clear-53545.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+            }]
+        }];
+
+})(window.maps = window.maps || {});
